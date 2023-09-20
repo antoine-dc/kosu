640e15c0fbf70bc9dc35832f27f3bd07e6078e73
antoine-dc <antoinedeconto@gmail.com>
2023-09-20 20:30:44 +0200

    Add auth

--- routes/students.js
+++ routes/students.js
@@ -1,13 +1,13 @@
 const express = require("express")
-
+const auth = require("../middleware/auth")
 const router = express.Router()
 
 const studentsCtrl = require("../controllers/students")
 
 router.post("/", studentsCtrl.createStudent)
-router.get("/:username", studentsCtrl.getStudent)
+router.get("/:username", auth, studentsCtrl.getStudent)
 router.post("/login", studentsCtrl.login)
-router.put("/", studentsCtrl.updateStudent)
-router.delete("/", studentsCtrl.deleteStudent)
+router.put("/", auth, studentsCtrl.updateStudent)
+router.delete("/", auth, studentsCtrl.deleteStudent)
 
 module.exports = router
