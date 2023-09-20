8d8f5827fafd8d89f6a8d2329ed1156ddb8ac470
antoine-dc <antoinedeconto@gmail.com>
2023-09-20 14:43:24 +0200

    Test change name file

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
-const User = require("./../models/User")
+const User = require("./../models/user.js")
 
 //fonction asynchrone de signup
 exports.signup = (req, res, next) => {
