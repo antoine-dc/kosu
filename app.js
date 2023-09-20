9557652da735125a004e2e339360d3f29b47bea0
antoine-dc <antoinedeconto@gmail.com>
2023-09-20 15:50:32 +0200

    Change code to test

--- app.js
+++ app.js
@@ -1,49 +1,34 @@
-const express = require("express") //import framework express
-const helmet = require("helmet") //package sécurité
-const mongoose = require("mongoose")
-const path = require("path")
-const rateLimit = require("express-rate-limit")
+const mongoose = require('mongoose')
+const express = require('express')
+require('dotenv').config()
 
-require("dotenv").config()
+const employeesRoutes = require('./routes/employees')
 
-//import des routeurs
-const userRoutes = require("./routes/user")
-const sauceRoutes = require("./routes/sauce")
-
-//connexion API à la BDD (MongoDB via mongoose)
 mongoose
-  .connect("mongodb+srv://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@" + process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"))
+    .connect(
+        process.env.DB_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
-const app = express() //création application express
+const app = express()
 
-app.use(helmet())
-
-//middleware d'ajout de headers à l'objet response pour éviter les erreurs de CORS
-app.use((req, res, next) => {
-  //accès à notre API depuis n'importe quelle origine
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  //ajout des headers mentionnés aux requêtes envoyées vers notre API
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization")
-  //envoi des requêtes avec les méthodes mentionnées
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
-  next()
-})
-
-//transformation du corps de la requete en json
 app.use(express.json())
 
-//gestionnaire de routage des ressources images
-app.use("/images", express.static(path.join(__dirname, "images")))
-
-//blocage tentative de connexion de force brute
-const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10,
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
+    next()
 })
 
-app.use("/api/auth", apiLimiter, userRoutes)
-app.use("/api/sauces", sauceRoutes)
+app.use('/api/employees', employeesRoutes)
 
-module.exports = app //export de l'app pour y accéder depuis les autres fichiers
+module.exports = app
